cfc5d483e5bfb9a81ee5b70034cdb77332ea8729
Oleg Golovanov <golovanov.ov@gmail.com>
2021-09-10 11:04:20 +0500

    Cli interface added by using clap crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use {
+    std::path::Path,
+
+    clap,
+};
+
+
+const DEFAULT_CONFIG_FILE: &str = "/etc/tsplay.conf";
+
+
+fn path_validator(path: String) -> Result<(), String> {
+    let file = Path::new(&path);
+    match file.exists() {
+        true => Ok(()),
+        false => Err(format!("cannot access '{}': No such file", path))
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = clap::App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("Play (stream) TS packets.")
+        .arg(clap::Arg::with_name("debug")
+            .short("d")
+            .long("debug")
+            .help("Enable debug"))
+        .arg(clap::Arg::with_name("config")
+            .takes_value(true)
+            .value_name("CONFIG")
+            .default_value(DEFAULT_CONFIG_FILE)
+            .validator(path_validator)
+            .help("config file")
+        ).get_matches();
+
+    dbg!(args);
 }
