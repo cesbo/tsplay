17380782e2ae60b14f5e0a053a9a134381cdbcf5
Oleg Golovanov <golovanov.ov@gmail.com>
2021-09-17 14:23:58 +0500

    Anyhow added as project dependency.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ mod config;
 
 use {
     std::{
-        fs::File,
+        fs,
         path::Path,
+    },
+
+    anyhow::{
+        Result,
+        Context,
     },
 
     config::Config,
@@ -17,12 +22,12 @@ fn path_validator(path: String) -> Result<(), String> {
     let file = Path::new(&path);
     match file.exists() {
         true => Ok(()),
-        false => Err(format!("cannot access '{}': No such file", path))
+        false => Err(format!("cannot access \"{}\": No such file", path))
     }
 }
 
 
-fn main() {
+fn main() -> Result<()> {
     let args = clap::App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -30,19 +35,23 @@ fn main() {
         .arg(clap::Arg::with_name("debug")
             .short("d")
             .long("debug")
-            .help("Enable debug"))
+            .help("enable debug"))
         .arg(clap::Arg::with_name("config")
             .takes_value(true)
             .value_name("CONFIG")
             .default_value(DEFAULT_CONFIG_FILE)
             .validator(path_validator)
-            .help("config file")
+            .help("configuration file")
         ).get_matches();
 
-    dbg!(&args);
-
-    let file = File::open(args.value_of_os("config").unwrap()).unwrap();
-    let config: Config = serde_json::from_reader(&file).unwrap();
+    // Unwrap use, because there is a default value and a validator for the config argument.
+    let path = args.value_of("config").unwrap();
+    let data = fs::read(&path)
+        .with_context(|| format!("Failed to read configuration file \"{}\"", &path))?;
+    let config: Config = serde_json::from_slice(&data)
+        .with_context(|| format!("Failed to parse configuration file \"{}\"", &path))?;
 
     dbg!(&config);
+
+    Ok(())
 }
